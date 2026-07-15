b5ccb5757fcb9bfd3b15bc25c0dae3254354fad4
idwahman <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-07-14 23:39:22 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name="MyApp",
+    version="1.0",
+    description="My Python App",
+    executables=[Executable("killing_fck_microslop_useless_process.py")]
+)
