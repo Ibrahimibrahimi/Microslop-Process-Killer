e38e6a687c9688b6596bff727ed5fb8a1748c3ad
idwahman <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-07-14 23:59:33 -0700

    Add files via upload

--- killing_fck_microslop_useless_process.py
+++ killing_fck_microslop_useless_process.py
@@ -5,7 +5,7 @@ import os
 # Configuration
 config_file = "settings.cfg"
 FROM_FILE = False
-PATH = None
+PATH = "empty"
 
 # Load configuration from file
 if os.path.exists(config_file):
@@ -21,20 +21,16 @@ else:
     print("No config file found. Proceeding with defaults.")
 
 # Default list of processes to kill
-tokill = ["TrustedInstaller.exe", "TiWorker.exe"]
+tokill = ["TrustedInstaller.exe", "TiWorker.exe","GoogleCrashHandler64.exe","GoogleCrashHandler.exe"]
 
 # Ask user if they want to load additional processes from a text file
-if FROM_FILE:
-    from_text = input("Use a text list file? (leave empty for No): ").strip()
-    if from_text != "":
-        path = input("Path for list of processes to kill: ").strip()
-        if os.path.exists(path):
-            with open(path, "r") as file:
-                content = [line.strip() for line in file.readlines()]
-                print(f"Loaded {len(content)} items!")
-                tokill.extend(content)
-        else:
-            print("Path not found. Continuing with default process list.")
+if os.path.exists(PATH):
+    with open(PATH, "r") as file:
+        content = [line.strip() for line in file.readlines()]
+        print(f"Loaded {len(content)} items!")
+        tokill.extend(content)
+else:
+    print("Path not found. Continuing with default process list.")
 
 def get_running_processes():
     """Return a list of tuples (name, pid) for all running processes."""
@@ -61,18 +57,16 @@ def kill_process(pid):
         print(f"An error occurred: {e}")
 
 def main():
-    """Main loop to monitor and kill specified processes."""
     while True:
         processes = get_running_processes()
         # Create a set of process names for faster lookup
         process_names = {name for name, _ in processes}
         for name, pid in processes:
+            # if in black list
             if name in tokill:
                 print(f"Found '{name}' (PID: {pid})! Attempting to kill...")
                 kill_process(pid)
-            else :
-                print("Not found ")
-                time.sleep(2)
+
         # Sleep for a while before next check
         time.sleep(3)
 
